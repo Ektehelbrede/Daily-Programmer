dcac192da87677fbc8b7ca9902c9fbe1b3860fc5
Michael Mckee <michael.mckee92@gmail.com>
2016-04-29 17:20:28 -0400

    Abstracting
    Made things look nicer in fewer lines. I think it's much more readable.

--- Python/[02122012] Challenge #4 [Easy] Password Generator/DP004PasswordGenerator.py
+++ Python/[02122012] Challenge #4 [Easy] Password Generator/DP004PasswordGenerator.py
@@ -5,7 +5,7 @@
 # V1.0 Generates a number of passwords of a certain length as specified by the user through command line arguments.
 # Outputs these generated passwords to a text file.
 
-import random
+from random import randint
 from sys import argv
 
 
@@ -19,15 +19,12 @@ lengthOfPasswords = int(argv[2])
 
 textFile = open('GeneratedPasswords.txt', 'a')
 
-currentNumber = 0
-while currentNumber < numberOfPasswords:
-    currentLength = 0
+
+for i in range(numberOfPasswords):
     password = ''
-    while currentLength < lengthOfPasswords:
-        password += chr(random.randint(33, 126))
-        currentLength += 1
+    for j in range(lengthOfPasswords):
+        password += chr(randint(33, 126))
     textFile.write(password + '\n')
-    currentNumber += 1
 
 print('Generated ' + str(numberOfPasswords) + ' passwords each of length ' + str(lengthOfPasswords) + '.')
 print('The passwords have been placed in GeneratedPasswords.txt')
